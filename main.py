de009cb25666eb4f51bc761603edf1098c4a1e32
Taryck CIDERE <tar.cidere@gmail.com>
2024-12-17 13:13:48 +0100

    Correction main

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
-from inserer import insert_words
-from suppression import delete_words
-from fusion import fusion_trie
-from listMots import list_Mots
-from prefixe import prefixe_mot
-from profondeurMoyenne import profondeurMoyenne
+from PatriciaTree.inserer import insert_words
+from PatriciaTree.suppression import delete_words
+from PatriciaTree.fusion import fusion_trie
+from PatriciaTree.listMots import list_Mots
+from PatriciaTree.prefixe import prefixe_mot
+from PatriciaTree.profondeurMoyenne import profondeurMoyenne
 
-from Projet_ALGAV.Tries_Hybrides.HybridTrie import *
+from TrieHybride.HybridTrie import *
 
 def main():
     # Vérifie que 3 ou 4 arguments sont fournis
@@ -30,46 +30,44 @@ def main():
         optional_param = sys.argv[4]  # Récupère le quatrième argument
 
     # Traitement en fonction de x, de l'action et de l'optional_param
-    for i in range(10) :
-
-        if x == 0:
-            if action == "inserer":
-                insert_words(filename)
-            elif action == "suppression":
-                delete_words(filename)
-            elif action == "listeMots":
-                list_Mots(filename)
-            elif action == "profondeurMoyenne":
-                profondeurMoyenne(filename)
-            elif action == "fusion":
-                if optional_param:
-                    fusion_trie(filename, optional_param)  # Exemple d'utilisation de l'optional_param
-                else:
-                    print("Erreur : L'action 'fusion' nécessite un paramètre supplémentaire (trie à fusionner).")
-            elif action =="prefixe":
-                if optional_param:
-                    prefixe_mot(filename, optional_param)  # Exemple d'utilisation de l'optional_param
-                else:
-                    print("Erreur : L'action 'prefixe' nécessite un paramètre supplémentaire (trie à fusionner).")
+    if x == 0:
+        if action == "inserer":
+            insert_words(filename)
+        elif action == "suppression":
+            delete_words(filename)
+        elif action == "listeMots":
+            list_Mots(filename)
+        elif action == "profondeurMoyenne":
+            profondeurMoyenne(filename)
+        elif action == "fusion":
+            if optional_param:
+                fusion_trie(filename, optional_param)  # Exemple d'utilisation de l'optional_param
             else:
-                print(f"Action non reconnue : {action}")
-        elif x == 1:
-            if action == "inserer":
-                insert_words_TH(filename)
-            elif action == "suppression":
-                delete_words_TH(filename)
-            elif action == "listeMots":
-                list_Mots_TH(filename)
-            elif action == "profondeurMoyenne":
-                profondeurMoyenne_TH(filename)
-            elif action == "fusion":
-                print("Erreur : Il n'a pas été demandé d'implémenter une fonction de fusion pour les trie hybride.")
-            elif action == "prefixe":
-                prefixe_TH(filename, optional_param)
+                print("Erreur : L'action 'fusion' nécessite un paramètre supplémentaire (trie à fusionner).")
+        elif action =="prefixe":
+            if optional_param:
+                prefixe_mot(filename, optional_param)  # Exemple d'utilisation de l'optional_param
             else:
-                print(f"Action non reconnue : {action}")
+                print("Erreur : L'action 'prefixe' nécessite un paramètre supplémentaire (trie à fusionner).")
         else:
-            print("Valeur de x non reconnue : x doit être 0 ou 1")
+            print(f"Action non reconnue : {action}")
+    elif x == 1:
+        if action == "inserer":
+            insert_words_TH(filename)
+        elif action == "suppression":
+            delete_words_TH(filename)
+        elif action == "listeMots":
+            list_Mots_TH(filename)
+        elif action == "profondeurMoyenne":
+            profondeurMoyenne_TH(filename)
+        elif action == "fusion":
+            print("Erreur : Il n'a pas été demandé d'implémenter une fonction de fusion pour les trie hybride.")
+        elif action == "prefixe":
+            prefixe_TH(filename, optional_param)
+        else:
+            print(f"Action non reconnue : {action}")
+    else:
+        print("Valeur de x non reconnue : x doit être 0 ou 1")
 
 if __name__ == "__main__":
     main()
